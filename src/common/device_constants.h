d9ff768825959c83d61b8215d363d4771f9652bb
DavideDeLeonardis <davidedeleonardis1711@gmail.com>
2025-11-09 16:38:21 +0100

    add device constant names

--- src/common/device_constants.h
+++ src/common/device_constants.h
@@ -5,10 +5,10 @@
  */
 namespace device {
 
+inline constexpr const char *CPU_FF = "cpu_ff";
+inline constexpr const char *CPU_OMP = "cpu_omp";
 inline constexpr const char *GPU_CL = "gpu_opencl";
 inline constexpr const char *GPU_MTL = "gpu_metal";
 inline constexpr const char *FPGA = "fpga";
-inline constexpr const char *CPU_FF = "cpu_ff";
-inline constexpr const char *CPU_OMP = "cpu_omp";
 
 } // namespace device
